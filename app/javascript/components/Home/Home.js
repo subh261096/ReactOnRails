f5abbeb61c6d37a82b088944c1878b2436f97eb8
subh261096 <subhashish.subhashish.singh@gmail.com>
2020-07-26 19:54:44 +0530

    setuped webpacker

---
+++ app/javascript/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+    constructor(){
+        
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Home
+
